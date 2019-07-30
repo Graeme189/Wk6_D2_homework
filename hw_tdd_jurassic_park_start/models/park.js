8eeaaa8a6104c88270e24b48c20b042b427ef203
Graeme Henderson <graeme.189@gmail.com>
2019-07-30 21:09:00 +0100

    Final commit

--- hw_tdd_jurassic_park_start/models/park.js
+++ hw_tdd_jurassic_park_start/models/park.js
@@ -4,26 +4,61 @@ const Park = function(name, price) {
   this.collection = [];
 }
 
-Park.prototype.addDinosaur = function(dinosaur, guests) {
+Park.prototype.addDinosaur = function(dinosaur) {
   this.collection.push(dinosaur);
 }
 
-Park.prototype.removeDinosaur = function () {
-  this.collection.pop;
+Park.prototype.removeDinosaur = function(dinosaur) {
+  this.collection.shift(dinosaur);
 }
 
 Park.prototype.mostVisitors = function () {
-  const sorted = this.collection.sort();
-  return sorted[0];
+  let findTopVisitors = this.collection.map((dinosaur) => dinosaur.guestsAttractedPerDay);
+  let topVisitors = Math.max.apply(null, findTopVisitors)
+  return topVisitors
 }
 
-// Park.prototype.mostVisitors = function () {
-//   let findTopVisitors = this.collection.map((dinosaur) => dinosaur.guestsAttractedPerDay);
-//   // Above is returning all visitor numbers as an array
-//   let topVisitors = Math.max.apply(null, findTopVisitors)
-//   // Above is then using math.max.apply to find the highest number in the new array
-//   // console.log(topVisitors);
-//   return topVisitors
-// }
+Park.prototype.findDinosaurVisitors = function (visitors) {
+  for (let dinosaur of this.collection) {
+    if (dinosaur.guestsAttractedPerDay === visitors){
+      return dinosaur
+    }
+  }
+}
 
+Park.prototype.findDinosaursBySpecies = function (species) {
+  const dinosaurSpecies = []
+  for (let dinosaur of this.collection){
+    if (dinosaur.species === species){
+      dinosaurSpecies.push(dinosaur.species)
+    }
+  }
+  return dinosaurSpecies
+}
+
+Park.prototype.totalVisitorsPerDay = function () {
+  let visitor_numbers = 0;
+  for (let dinosaur of this.collection) {
+    visitor_numbers += dinosaur.guestsAttractedPerDay;
+  };
+  return visitor_numbers;
+};
+
+Park.prototype.totalVisitorsPerYear = function () {
+  let visitor_numbers = 0;
+  for (let dinosaur of this.collection) {
+  visitor_numbers += dinosaur.guestsAttractedPerDay;
+ };
+ return visitor_numbers * 365;
+};
+
+
+Park.prototype.totalTicketSalesPerYear = function () {
+  let ticket_sales = 0;
+  let visitor_numbers = 0;
+  for (let dinosaur of this.collection) {
+    visitor_numbers += dinosaur.guestsAttractedPerDay;
+  };
+  return (visitor_numbers * 365) * this.price;
+  };
 module.exports = Park
--- hw_tdd_jurassic_park_start/specs/park_spec.js
+++ hw_tdd_jurassic_park_start/specs/park_spec.js
@@ -3,6 +3,10 @@ const Park = require('../models/park.js');
 const Dinosaur = require('../models/dinosaur.js');
 
 describe('Park', function() {
+let park;
+let dinosaur1;
+let dinosaur2;
+let dinosaur3;
 
   beforeEach(function () {
     park = new Park('Triassic Bark', 20)
@@ -27,24 +31,60 @@ describe('Park', function() {
 
   });
 
-  // it('should be able to add a dinosaur to its collection', function () {
-  //   park.addDinosaur('Triceratops');
-  //   const actual = park.collection.length;
-  //   assert.strictEqual(actual, 1);
-  // });
+  it('should be able to add a dinosaur to its collection', function () {
+    park.addDinosaur(dinosaur1);
+    const actual = park.collection;
+    assert.deepEqual(actual, [{"diet": "carnivore", "guestsAttractedPerDay": 50, "species": "T-Rex"}]);
+  })
 
 
-  // it('should be able to remove a dinosaur from its collection', function () {
-  //   park.removeDinosaur();
-  //   const actual = park.collection.length;
-  //   assert.strictEqual(actual, 0);
-  // });
+  it('should be able to remove a dinosaur from its collection', function () {
+    park.addDinosaur(dinosaur1);
+    park.removeDinosaur(dinosaur1);
+    const actual = park.collection;
+    assert.deepStrictEqual(actual, []);
+  });
 
-  //
-  // it('should be able to find the dinosaur that attracts the most visitors');
-  //
-  // it('should be able to find all dinosaurs of a particular species');
-  //
-  // it('should be able to remove all dinosaurs of a particular species');
+
+  it('should be able to find the dinosaur that attracts the most visitors', function () {
+    park.addDinosaur(dinosaur1)
+    park.addDinosaur(dinosaur2)
+    park.addDinosaur(dinosaur3)
+    const actual = park.findDinosaurVisitors(park.mostVisitors())
+    assert.strictEqual(actual, dinosaur1)
+  });
+
+  it('should be able to find all dinosaurs of a particular species', function () {
+    park.addDinosaur(dinosaur1)
+    park.addDinosaur(dinosaur2)
+    park.addDinosaur(dinosaur3)
+    const actual = park.findDinosaursBySpecies('Pterodactyl')
+    assert.deepStrictEqual(actual, ['Pterodactyl'])
+  });
+
+  it('should calculate the total number of visitors per day', function (){
+    park.addDinosaur(dinosaur1);
+    park.addDinosaur(dinosaur2);
+    park.addDinosaur(dinosaur3);
+    const actual = park.totalVisitorsPerDay();
+    assert.strictEqual(actual, 100);
+  })
+
+  it('should calculate the total number of visitors per year', function(){
+    park.addDinosaur(dinosaur1);
+    park.addDinosaur(dinosaur2);
+    park.addDinosaur(dinosaur3);
+    const actual = park.totalVisitorsPerYear();
+    assert.strictEqual(actual, 36500);
+  })
+
+  it('should calculate total ticket sales for one year', function(){
+    park.addDinosaur(dinosaur1)
+    park.addDinosaur(dinosaur2)
+    park.addDinosaur(dinosaur3)
+    const actual = park.totalTicketSalesPerYear()
+    assert.strictEqual(actual, 730000)
+  })
+
 
 });
